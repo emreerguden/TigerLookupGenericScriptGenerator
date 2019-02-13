ab3850fcd37ce627774034fecf20ff9a320f2233
Emre Erguden <Emre.Erguden@logo.com.tr>
2019-02-13 15:35:17 +0300

    ..

---
+++ TigerLookupDataChecker/Models/Factories/BankBranchFetcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TigerLookupDataChecker.Models.Factories.Base;
+using TigerLookupDataChecker.Helpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TigerLookupDataChecker.Models.Factories
+{
+    public class BankBranchFetcher : LookupBaseFetcher
+    {
+
+        public override dynamic GetInputFileData(string fileName)
+        {
+            return GetDataFromInputFile(fileName);
+        }
+
+        public BankBranchXml GetDataFromInputFile(string fileName)
+        {
+            BankBranchXml bankBranches = new BankBranchXml();
+            try
+            {
+                bankBranches = Utils.DeSerialize<BankBranchXml>(fileName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return bankBranches;
+        }
+
+        public override Task<List<CompareFileLine>> FetchAndGetComparisonData(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveNewDataToOutputFile(string targetFile, List<CompareFileLine> records, bool openTargetDirectory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TigerLookupDataChecker/Models/Factories/BankOfXmlFetcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TigerLookupDataChecker.Models.Factories.Base;
+using TigerLookupDataChecker.Helpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TigerLookupDataChecker.Models.Factories
+{
+    public class BankOfXmlFetcher : LookupBaseFetcher
+    {
+        public override dynamic GetInputFileData(string fileName)
+        {
+            return GetDataFromInputFile(fileName);
+        }
+
+        public BankXml GetDataFromInputFile(string fileName)
+        {
+            BankXml banks = new BankXml();
+            try
+            {
+                banks = Utils.DeSerialize<BankXml>(fileName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return banks;
+        }
+
+        public override Task<List<CompareFileLine>> FetchAndGetComparisonData(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveNewDataToOutputFile(string targetFile, List<CompareFileLine> records, bool openTargetDirectory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
